c3352c48e4205d93c2708d0472344432c2c837e8
parmeshragave-rgb <parmeshragav.e@rently.com>
2025-10-16 09:28:28 +0530

    Initial commit: React Vite project setup

---
+++ src/course.jsx
@@ -0,0 +1,11 @@
+function Course(){
+    return(
+        <div>
+            <img src="" alt="" />
+            <h3>HTML</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis voluptas harum excepturi iste voluptatibus doloremque eveniet tempore eaque eos cupiditate.</p>
+        </div>
+    )
+}
+
+export default Course
---
+++ src/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar(){
+    return(
+        <nav>
+            <h1>Abc.net</h1>
+            <button>login</button>
+        </nav>
+    )
+}
+export default Navbar
